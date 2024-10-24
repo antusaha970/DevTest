eeaf7d375aa2c806f15c270d1fc97d6b6ac9752a
Antu Saha <antusaha970@gmail.com>
2024-10-24 20:42:04 +0600

    formated the email

--- devTest/settings.py
+++ devTest/settings.py
@@ -1,4 +1,7 @@
 from pathlib import Path
+import environ
+env = environ.Env()
+environ.Env.read_env()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -93,3 +96,10 @@ STATIC_URL = 'static/'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
--- fileProcess/views.py
+++ fileProcess/views.py
@@ -3,12 +3,28 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import CustomFileSerializer
 import pandas as pd
+from django.core.mail import send_mail
 
 
 def generate_summary(data):
     summary = data.groupby(['Cust State', 'Cust Pin'])[
         'DPD'].agg(['sum', 'count']).reset_index()
+    summary = summary.rename(
+        columns={'sum': 'Total DPD', 'count': 'Count of Records'})
     return summary.to_dict(orient='records')
+
+
+def send_summary_mail(summary):
+    message = "Summary Report:\n\n"
+    message += "{}\t {}\t {}\t {}\n".format("Cust State",
+                                            "Cust Pin", "Total DPD", 'Count of Records')
+    message += "-" * 80 + "\n"
+    for row in summary:
+        message += "{}\t {}\t {}\t {}\n".format(
+            row['Cust State'], row['Cust Pin'], row['Total DPD'], row['Count of Records'])
+
+    send_mail('Python Assignment - Antu Saha', message,
+              'antu.digi.88@gmail.com', ['antusaha990@gmail.com'])
 
 
 class FileProcessView(APIView):
@@ -18,6 +34,7 @@ class FileProcessView(APIView):
             file = serializer.save()
             data = pd.read_excel(file.file.path)
             summary = generate_summary(data)
+            send_summary_mail(summary)
             return Response(summary)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
