df6e5de983d7c8cf629acacab831df425c5c51bd
Antu Saha <antusaha970@gmail.com>
2024-10-24 20:13:49 +0600

    added pandas for generating summary in view

--- fileProcess/views.py
+++ fileProcess/views.py
@@ -1,13 +1,23 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from .serializers import CustomFileSerializer
+import pandas as pd
+
+
+def generate_summary(data):
+    summary = data.groupby(['Cust State', 'Cust Pin'])[
+        'DPD'].agg(['sum', 'count']).reset_index()
+    return summary.to_dict(orient='records')
 
 
 class FileProcessView(APIView):
     def post(self, request):
         serializer = CustomFileSerializer(data=request.data)
         if serializer.is_valid():
-            return Response("ok")
+            file = serializer.save()
+            data = pd.read_excel(file.file.path)
+            summary = generate_summary(data)
+            return Response(summary)
 
-        return Response(serializer.errors)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
